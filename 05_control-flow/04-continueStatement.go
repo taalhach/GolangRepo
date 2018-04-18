905f01145f383bb38f85fcf055d2e926f101e79d
taalhach <taalhach891@gmail.com>
2018-04-18 12:45:24 +0500

    name without vowel game

---
+++ 05_control-flow/04-continueStatement.go
@@ -0,0 +1,28 @@
+package main
+import (
+	"bufio"
+	"os"
+	"fmt"
+)
+func main() {
+	nameWithoutVowel:=""
+	reader:=bufio.NewReader(os.Stdin)
+	fmt.Println("Enter your name")
+	userName,_:=reader.ReadString('\n')
+	userName=string(userName)
+	vowel:=[]rune{'a','e','i','o','u'}
+
+	for _,nameChar:=range userName{
+		isVowel:=false
+		for _,vow:= range vowel {
+			if(vow==nameChar){
+				isVowel=true
+			}
+		}
+		if(isVowel){
+			continue
+		}
+		nameWithoutVowel+=string(nameChar)
+	}
+	fmt.Println("your name without vowels: ",nameWithoutVowel)
+}
