4fb65ce4a9b3fb532ad5bad7c447c5205ca5138e
taalhach <taalhach891@gmail.com>
2018-04-20 10:39:46 +0500

    rune literals

--- 05_control-flow/05_rune.go
+++ 05_control-flow/05_rune.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main()  {
-	// rune is literal in which every char is represented by a number that can be ASCII
+	// rune is literal in which every char is represented by a number that is ASCII code
 	chr :=rune('a')
 	fmt.Println("ASCI code",chr)
 	fmt.Println(string(chr))
