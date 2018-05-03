1d3636e4f44cc7fe7b0a9b6e73149d7d90debb6b
taalhach <taalhach891@gmail.com>
2018-05-03 20:01:09 +0500

    Defers

--- 06_functions/07_defer_statement/01_defer_fileSystem.go
+++ 06_functions/07_defer_statement/01_defer_fileSystem.go
@@ -9,7 +9,7 @@ import (
 func main()  {
 
 	//if you are using goland then file path given from root
-	fileSrc,err:=os.Open("file.txt")
+	fileSrc,err:=os.Open("/home/resilient/go/src/github.com/taalhach/GolangRepo/06_functions/07_defer_statement/file.txt")
 	if(err!=nil){
 		fmt.Print(err)
 		panic("404 file not found")
