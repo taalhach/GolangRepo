51cda8d9ab384c5dd4d1b91e4dcb56d9eacfcb1b
taalhach <talhach891@gmail.com>
2018-07-07 17:06:33 +0500

    Embeding and anonymous fields

--- 10_Structs/03_anonymous_fields/02_another_example/main.go
+++ 10_Structs/03_anonymous_fields/02_another_example/main.go
@@ -28,9 +28,17 @@ func main()  {
 		"Sp15-BCS-042",
 		2017,
 	}
+	// second way of initializing structs
+	stdB:=Student{
+		Person:Person{Name:"Ali Raza",Age:27},
+		RegNo:"abc",
+		Enrollment:2015,
+
+	}
 
 	fmt.Println(stdA.Name,stdA.Age)
 
 	fmt.Println(stdA)
+	fmt.Println(stdB)
 
 }
