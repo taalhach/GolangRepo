3f2cbeadb48e0ca7fc1918d10c61bd3184d5d73c
taalhach <talhach891@gmail.com>
2018-07-08 21:18:01 +0500

    Marshal Maps and structs

---
+++ 10_Structs/04_Json_marshal/04_maps_marshal/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"fmt"
+)
+
+type Orderqueue struct {
+	Name string
+	Orders map[int]string
+
+}
+// we can only use int and string as key of map in marshalling otherwise it throws an error 
+var m =make(map[int]string)
+
+func main()  {
+	m[1011]="order 1"
+	m[101]="order 2"
+	m[101]="order 3"
+	m[101]="order 4"
+	ent:=Orderqueue{
+		Name:"AltEx",
+		Orders:m,
+	}
+	mdata,err:=json.MarshalIndent(ent,""," ")
+	if err!=nil{
+		fmt.Println(err)
+		log.Fatal("Error occured")
+
+	}
+	fmt.Println(string(mdata))
+
+}
