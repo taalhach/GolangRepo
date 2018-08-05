335f7892a68911a345c7efa7e72120e1f99a18b1
taalhach <talhach891@gmail.com>
2018-08-05 19:56:10 +0500

    Multiple Random selection

--- 12_channels/12_select_statement_defaultCase/main.go
+++ 12_channels/12_select_statement_defaultCase/main.go
@@ -1,8 +1,6 @@
 package main
-
 import (
 	"fmt"
-	"time"
 )
 
 var balanes =map[string]float64{
@@ -12,15 +10,10 @@ var balanes =map[string]float64{
 }
 
 func BalancerReponse(Uid string,ch chan<- float64)  {
-	time.Sleep(time.Millisecond*1000)
 	ch<-balanes[Uid]
 }
 
 func DatabaseReponse(Uid string,ch chan<- float64)  {
-	time.Sleep(time.Millisecond*6000)
-	fmt.Println("Conecting db")
-	fmt.Println("Finding result")
-	time.Sleep(time.Millisecond*1000)
 	ch<-balanes[Uid]
 }
 
@@ -32,11 +25,8 @@ func main() {
 	go BalancerReponse(userId,blncr)
 	select {
 	case b:=<-blncr:
-		fmt.Printf("User %v has %v balance",userId,b)
+		fmt.Printf("User %v has %v balance \n Response from balancer",userId,b)
 	case b := <-db:
-		fmt.Printf("User %v has %v balance",userId,b)
-	default:
-		fmt.Println("Couldn't wait that much long ")
+		fmt.Printf("User %v has %v balance \n Response from DB",userId,b)
 	}
-
 }
