d361893199e6683cccf461fb79f1ff5682a8a83b
taalhach <talhach891@gmail.com>
2018-08-05 19:44:29 +0500

    Channel select statement

---
+++ 12_channels/11_select_Statement/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var balanes =map[string]float64{
+	"abc":0.333,
+	"a":1,
+	"b":2,
+}
+
+func BalancerReponse(Uid string,ch chan<- float64)  {
+	time.Sleep(time.Millisecond*1000)
+	ch<-balanes[Uid]
+}
+
+func DatabaseReponse(Uid string,ch chan<- float64)  {
+	time.Sleep(time.Millisecond*6000)
+	fmt.Println("Conecting db")
+	fmt.Println("Finding result")
+	time.Sleep(time.Millisecond*1000)
+	ch<-balanes[Uid]
+}
+
+func main() {
+	blncr:=make(chan float64)
+	db:=make(chan float64)
+	userId:="abc"
+	go DatabaseReponse(userId,db)
+	go BalancerReponse(userId,blncr)
+	select {
+	case b:=<-blncr:
+		fmt.Printf("User %v has %v balance",userId,b)
+	case b := <-db:
+		fmt.Printf("User %v has %v balance",userId,b)
+	}
+}
