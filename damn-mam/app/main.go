945d7aaf29db5b897896efeb91bdc6c67270f7e0
taalhach <talhach891@gmail.com>
2018-08-10 04:02:28 +0500

    wah that is last comment

--- damn-mam/app/main.go
+++ damn-mam/app/main.go
@@ -4,7 +4,7 @@ import (
 	"github.com/taalhach/01_GolangRepo/damn-mam"
 	"fmt"
 )
-var a damn_mam.Outer="sangi again changing"
+var a damn_mam.Outer="sangi again changing f"
 func main()  {
 	fmt.Println(b)
 	fmt.Println(damn_mam.D)
